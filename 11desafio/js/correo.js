93b1fa61e7f66ae251efb2f662ab4e95c321b127
Juan Zava <jmzava@gmail.com>
2021-10-17 19:24:55 -0300

    2

--- 11desafio/js/correo.js
+++ 11desafio/js/correo.js
@@ -1,111 +1,42 @@
+service_id: 'service_46usxmj';
+template_id: 'template_murrdmt';
 
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '458054905886-u0datbbth57nppl7f60n99eei9igebai.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyCh_DNyR1FqEJVF_BcY1y3yyaABWP0o2DM';
+// function logMail() {
+//   emailjs.init("user_j2TxB8ZQtFXXf8WKVEqYO");
+//   };
+
+// function enviarCorreo(parametros){
+//   var tempParametros = 
+//       from_name= "buceocoder@gmail.com"
+//       to_name="jmzava@gmail.com"
+//       message="mensaje de prueba"
+// }
+
+// emailjs.sendForm(serviceID, templateID, tempParametros)
+// .then(function(res){
+//     console.log("enviado",res.status);
+// })
+btnEnvioCorreo.addEventListener("click", envioCorreo);
 
 
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
-
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-  }, function(error) {
-    appendPre(JSON.stringify(error, null, 2));
-  });
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    authorizeButton.style.display = 'none';
-    signoutButton.style.display = 'block';
-    listLabels();
-  } else {
-    authorizeButton.style.display = 'block';
-    signoutButton.style.display = 'none';
+function envioCorreo(){
+var data = {
+  service_id: 'service_46usxmj',
+  template_id: 'template_murrdmt',
+  user_id: 'buceo',
+  template_params: {
+      'username': 'BUCEOCODER',
+      'to_name':'jmzava@gmail.com'
   }
+};
+
+$.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+  type: 'POST',
+  data: JSON.stringify(data),
+  contentType: 'application/json'
+}).done(function() {
+  alert('Your mail is sent!');
+}).fail(function(error) {
+  alert('Oops... ' + JSON.stringify(error));
+})
 }
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-  var pre = document.getElementById('content');
-  var textContent = document.createTextNode(message + '\n');
-  pre.appendChild(textContent);
-}
-
-/**
- * Print all Labels in the authorized user's inbox. If no labels
- * are found an appropriate message is printed.
- */
-function listLabels() {
-  gapi.client.gmail.users.labels.list({
-    'userId': 'me'
-  }).then(function(response) {
-    var labels = response.result.labels;
-    appendPre('Labels:');
-
-    if (labels && labels.length > 0) {
-      for (i = 0; i < labels.length; i++) {
-        var label = labels[i];
-        appendPre(label.name)
-      }
-    } else {
-      appendPre('No Labels found.');
-    }
-  });
-}
-
-
-
